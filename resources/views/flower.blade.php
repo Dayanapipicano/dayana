8f8a06e296ce0a3e1d2d0d3705658f7d3ebf6979
laravelBasic <pipicanodayana2005@gmail.com>
2024-01-10 23:12:39 -0500

    Cambios

--- resources/views/flower.blade.php
+++ resources/views/flower.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="icon" href=" {{ asset('img/flowers.png') }}" type="image/x-icon" />
     <title>Flower</title>
   </head>
