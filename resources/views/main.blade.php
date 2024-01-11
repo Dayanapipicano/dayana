b3b31b3c71fb2b62a5729118fa77c61e60046d04
laravelBasic <pipicanodayana2005@gmail.com>
2024-01-10 22:17:34 -0500

    Cambios

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/public/css/style.css') }}">
+
+    <link rel="stylesheet" href="/public/css/style.css">
 
     <link rel="icon" href="{{ asset('img/flowers.png') }}" type="image/x-icon">
 
