69c1ca28c5e0008e1c6983f06e42a4185cb5898b
laravelBasic <pipicanodayana2005@gmail.com>
2024-01-10 21:44:54 -0500

    Cambios

---
+++ app/Http/Controllers/Flower.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Flower extends Controller
+{
+
+
+
+
+    public function flowers(){
+        return view('flower');
+    }
+    public function main(){
+        return view('main');
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Flower;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('flowers', [Flower::class,'flowers'])->name('flowers');
+Route::post('main', [Flower::class,'main'])->name('main');
