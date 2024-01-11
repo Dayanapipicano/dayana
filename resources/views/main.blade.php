0432e4f099d2e200d438104d0d554d67e213162f
laravelBasic <pipicanodayana2005@gmail.com>
2024-01-10 23:05:22 -0500

    Cambiosss

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="{{ asset('css/contact.css') }}" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <link rel="stylesheet" href="{{ asset('css/estilos.css') }}" />
 
     <link rel="icon" href="{{ asset('img/flowers.png') }}" type="image/x-icon">
